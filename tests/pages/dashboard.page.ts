95db600299ee4ac565176015dbf700ce2df50b33
Guruabishake <guruabishake.a@6solve.com>
2026-02-10 11:26:20 +0530

    latest code

--- tests/pages/dashboard.page.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class DashboardPage {
-  constructor(private page: Page) {}
-async goToMaster() {
-  await this.page.getByRole('button', { name: 'Master' }).click();
-
-}
-}
